1acb86234457f43c27f46077708504861475ea84
Manuj Bhalla <mbbhalla@amazon.com>
2019-10-07 16:44:36 +0530

    Inital commit with SQS and SNS

---
+++ aws-cdk-app-ts/bin/aws-cdk-app.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { AwsCdkAppStack } from '../lib/aws-cdk-app-stack';
+
+const app = new cdk.App();
+new AwsCdkAppStack(app, 'AwsCdkAppStack', {
+    env: {
+        region: 'ap-south-1',
+        account: '123456789101'
+    }
+});
